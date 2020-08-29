5619d7c3e741ac6b8136b977701fc8a2ee0c1bc0
Noah Stinson <noahstjohn@ymail.com>
2020-08-29 18:04:55 -0500

    game window created

---
+++ view/ClickerSimulator.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class ClickerSimulator {
+
+	private JFrame window;
+	
+	public ClickerSimulator(JFrame window) {
+		this.window = window;
+	}
+
+	public void init() {
+
+	}
+
+}
