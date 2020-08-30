1489812d246bff6719c440e7dce2a1b227eaaa7f
Noah Stinson <noahstjohn@ymail.com>
2020-08-30 18:20:04 -0500

    added has(PowerDegree) & getters/setters

--- model/Clicker.java
+++ model/Clicker.java
@@ -30,7 +30,12 @@ public class Clicker {
 	private double multiplier = 0; // for passive click income
 	private int clickPower = 1; // how many click $ per 
 
-	private PassiveClicks[] passiveClickInfo;
+	static PassiveClicks[] passiveClickInfo = new PassiveClicks[4]; // tracks passiveClick price & count
+
+	private boolean hasCert = false;
+	private boolean hasBS = false;
+	private boolean hasMS = false;
+	private boolean hasPHD = false;
 	
 	public void clicked() {
 		totalClicks++;
@@ -42,12 +47,16 @@ public class Clicker {
 		multiplier += multIncrease;
 	}
 	
-	void increaseClickPower(int powerIncrease) {
+	public void increaseClickPower(int powerIncrease) {
 		clickPower += powerIncrease;
 	}
 
 	public double getWallet() {
 		return wallet;
+	}
+
+	public void subtWallet(double subtAmount) {
+		wallet -= subtAmount;
 	}
 
 	public double getWalletTotal() {
@@ -66,10 +75,49 @@ public class Clicker {
 		return clickPower;
 	}
 
-	public PassiveClicks[] getPassiveClickInfo() {
+	static public PassiveClicks[] getPassiveClickInfo() {
 		return passiveClickInfo;
 	}
 
-	//public initPrices
+	//sets the price of each passiveClickIncome 
+	public void initPassiveClickInfo() {
+		for (int i = 0; i < 4; i++) 
+			passiveClickInfo[i] = new PassiveClicks();
+		
+		passiveClickInfo[0].setPrice(30);
+		passiveClickInfo[1].setPrice(500);
+		passiveClickInfo[2].setPrice(1000);
+		passiveClickInfo[3].setPrice(5000);
+	}
+
+	public boolean getHasPower(int index) {
+		switch (index) {
+			case 0:
+				return hasCert;
+			case 1:
+				return hasBS;
+			case 2:
+				return hasMS;
+			case 3:
+				return hasPHD;
+			default:
+				return false;
+		}
+	}
+
+	public void setHasPower(int index, boolean value) {
+		switch (index) {
+			case 0:
+				hasCert = value;
+			case 1:
+				hasBS = value;
+			case 2:
+				hasMS = value;
+			case 3:
+				hasPHD = value;
+		}
+	}
+
+
 	
 }
