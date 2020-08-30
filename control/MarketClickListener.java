3f5321dff34bd90b496ab174fd8c7c3ea178bad0
Noah Stinson <noahstjohn@ymail.com>
2020-08-30 18:20:56 -0500

    added purchasable buttons for power items

--- control/MarketClickListener.java
+++ control/MarketClickListener.java
@@ -18,7 +18,30 @@ public class MarketClickListener implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		var button = e.getSource();
 
-		//if (button )
+		if (button == panel.getClickCertButton() && panel.getClicker().getWallet() >= 30) {
+			panel.getClicker().setHasPower(0, true);
+			panel.getClicker().increaseClickPower(100); // *** need to change later
+			panel.getClicker().subtWallet(30);
+			panel.getClickCertButton().setEnabled(false);
+		}
+		else if (button == panel.getClickBSButton() && panel.getClicker().getWallet() >= 500) {
+			panel.getClicker().setHasPower(1, true);
+			panel.getClicker().increaseClickPower(1000); // *** need to change later
+			panel.getClicker().subtWallet(500);
+			panel.getClickBSButton().setEnabled(false);
+		}
+		else if (button == panel.getClickMSButton() && panel.getClicker().getWallet() >= 5000) {
+			panel.getClicker().setHasPower(2, true);
+			panel.getClicker().increaseClickPower(5000); // *** need to change later
+			panel.getClicker().subtWallet(5000);
+			panel.getClickMSButton().setEnabled(false);
+		}
+		else if (button == panel.getClickPHDButton() && panel.getClicker().getWallet() >= 10000) {
+			panel.getClicker().setHasPower(3, true);
+			panel.getClicker().increaseClickPower(10000); // *** need to change later
+			panel.getClicker().subtWallet(10000);
+			panel.getClickPHDButton().setEnabled(false);
+		}
 
 	}
 	
