585f05e72c80d1eb41e77b227ed0a5fd3b24f6f3
Noah Stinson <noahstjohn@ymail.com>
2020-09-01 13:51:19 -0500

    wallet update info for MarketScreen

--- control/MarketClickListener.java
+++ control/MarketClickListener.java
@@ -22,52 +22,66 @@ public class MarketClickListener implements ActionListener{
 		var button = e.getSource();
 		double currentWallet = panel.getClicker().getWallet();
 
-		if (button == panel.getClickCertButton() && currentWallet >= 30) {
+		if (button == panel.getClickCertButton() && currentWallet >= 100) {
 			panel.getClicker().setHasCert(true);
-			panel.getClicker().increaseClickPower(100); // *** need to change later
-			panel.getClicker().subtWallet(30);
+			panel.getClicker().setClickPower(5); // *** need to change later
+			panel.getClicker().subtWallet(100);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
 			panel.getClickCertButton().setEnabled(false);
 		}
-		if (button == panel.getClickBSButton() && currentWallet >= 500) {
+		if (button == panel.getClickBSButton() && currentWallet >= 1000) {
 			panel.getClicker().setHasBS(true);
-			panel.getClicker().increaseClickPower(1000); // *** need to change later
-			panel.getClicker().subtWallet(500);
+			panel.getClicker().setClickPower(15); // *** need to change later
+			panel.getClicker().subtWallet(1000);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
+			panel.getClickCertButton().setEnabled(false);
 			panel.getClickBSButton().setEnabled(false);
 		}
-		if (button == panel.getClickMSButton() && currentWallet >= 5000) {
+		if (button == panel.getClickMSButton() && currentWallet >= 10000) {
 			panel.getClicker().setHasMS(true);
-			panel.getClicker().increaseClickPower(5000); // *** need to change later
-			panel.getClicker().subtWallet(5000);
+			panel.getClicker().setClickPower(50); // *** need to change later
+			panel.getClicker().subtWallet(10000);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
+			panel.getClickCertButton().setEnabled(false);
+			panel.getClickBSButton().setEnabled(false);
 			panel.getClickMSButton().setEnabled(false);
 		}
-		if (button == panel.getClickPHDButton() && currentWallet >= 10000) {
+		if (button == panel.getClickPHDButton() && currentWallet >= 50000) {
 			panel.getClicker().setHasPHD(true);
-			panel.getClicker().increaseClickPower(10000); // *** need to change later
-			panel.getClicker().subtWallet(10000);
+			panel.getClicker().setClickPower(100); // *** need to change later
+			panel.getClicker().subtWallet(50000);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
+			panel.getClickCertButton().setEnabled(false);
+			panel.getClickBSButton().setEnabled(false);
+			panel.getClickMSButton().setEnabled(false);
 			panel.getClickPHDButton().setEnabled(false);
 		}
 		if (button == panel.getClickCatButton() && currentWallet >= Clicker.passiveClickInfo[0].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[0].getPrice());
 			Clicker.passiveClickInfo[0].purchased();
 			panel.getClicker().updateTimer(0.1);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
 			panel.getClickCatButton().setText("<html>Click Cat | Count: " + Clicker.getPassiveClickInfo()[0].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[0].getPrice() + "</html>");
 		}
 		if (button == panel.getClickCoinMinerButton() && currentWallet >= Clicker.passiveClickInfo[1].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[1].getPrice());
 			Clicker.passiveClickInfo[1].purchased();
 			panel.getClicker().updateTimer(1);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
 			panel.getClickCoinMinerButton().setText("<html>Click-Coin Miner | Count: " + Clicker.getPassiveClickInfo()[1].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[1].getPrice() + "</html>");
 		}
 		if (button == panel.getClickIncButton() && currentWallet >= Clicker.passiveClickInfo[2].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[2].getPrice());
 			Clicker.passiveClickInfo[2].purchased();
-			panel.getClicker().updateTimer(10);
+			panel.getClicker().updateTimer(5);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
 			panel.getClickIncButton().setText("<html>Click Incorporated® | Count: " + Clicker.getPassiveClickInfo()[2].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[2].getPrice() + "</html>");
 		}
 		if (button == panel.getClickMutantsButton() && currentWallet >= Clicker.passiveClickInfo[3].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[3].getPrice());
 			Clicker.passiveClickInfo[3].purchased();
-			panel.getClicker().updateTimer(100);
+			panel.getClicker().updateTimer(20);
+			panel.getWalletText().setText("Wallet: " + (double) (Math.round(panel.getClicker().getWallet() * 10) / 10.0));
 			panel.getClickMutantsButton().setText("<html>Click Mutants | Count: " + Clicker.getPassiveClickInfo()[3].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[3].getPrice() + "</html>");
 		}
 	}
