ec736bc05280b0508e97b5ffea2df30f29098aa6
Noah Stinson <noahstjohn@ymail.com>
2020-09-01 12:05:21 -0500

    change updateTimer parameter

--- control/MarketClickListener.java
+++ control/MarketClickListener.java
@@ -49,25 +49,25 @@ public class MarketClickListener implements ActionListener{
 		if (button == panel.getClickCatButton() && currentWallet >= Clicker.passiveClickInfo[0].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[0].getPrice());
 			Clicker.passiveClickInfo[0].purchased();
-			panel.getClicker().updateTimer(Clicker.passiveClickInfo[0].getCount() * 0.1);
+			panel.getClicker().updateTimer(0.1);
 			panel.getClickCatButton().setText("<html>Click Cat | Count: " + Clicker.getPassiveClickInfo()[0].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[0].getPrice() + "</html>");
 		}
 		if (button == panel.getClickCoinMinerButton() && currentWallet >= Clicker.passiveClickInfo[1].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[1].getPrice());
 			Clicker.passiveClickInfo[1].purchased();
-			panel.getClicker().updateTimer(Clicker.passiveClickInfo[1].getCount() * 1);
+			panel.getClicker().updateTimer(1);
 			panel.getClickCoinMinerButton().setText("<html>Click-Coin Miner | Count: " + Clicker.getPassiveClickInfo()[1].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[1].getPrice() + "</html>");
 		}
 		if (button == panel.getClickIncButton() && currentWallet >= Clicker.passiveClickInfo[2].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[2].getPrice());
 			Clicker.passiveClickInfo[2].purchased();
-			panel.getClicker().updateTimer(Clicker.passiveClickInfo[2].getCount() * 10);
+			panel.getClicker().updateTimer(10);
 			panel.getClickIncButton().setText("<html>Click Incorporated® | Count: " + Clicker.getPassiveClickInfo()[2].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[2].getPrice() + "</html>");
 		}
 		if (button == panel.getClickMutantsButton() && currentWallet >= Clicker.passiveClickInfo[3].getPrice()) {
 			panel.getClicker().subtWallet(Clicker.passiveClickInfo[3].getPrice());
 			Clicker.passiveClickInfo[3].purchased();
-			panel.getClicker().updateTimer(Clicker.passiveClickInfo[3].getCount() * 100);
+			panel.getClicker().updateTimer(100);
 			panel.getClickMutantsButton().setText("<html>Click Mutants | Count: " + Clicker.getPassiveClickInfo()[3].getCount() + "<br/> Price: " + Clicker.getPassiveClickInfo()[3].getPrice() + "</html>");
 		}
 	}
